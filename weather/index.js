01967b8962be8b44db84564520597cbf30eaf251
Higor Feijó <84149293+FeijoHigor@users.noreply.github.com>
2022-07-14 19:57:41 -0300

    Add files via upload

--- weather/index.js
+++ weather/index.js
@@ -4,6 +4,8 @@ const outTemp = document.getElementsByClassName('real-temp')[0]
 const outFell = document.getElementsByClassName('feel-temp')[0]
 const outMin = document.getElementsByClassName('min-temp')[0]
 const outMax = document.getElementsByClassName('max-temp')[0]
+const outHour = document.getElementsByClassName('hours')[0]
+const outDate = document.getElementsByClassName('date')[0]
 
 
 document.addEventListener('keydown', async (e) => {
@@ -33,12 +35,32 @@ async function test() {
         console.log('Cidade não encontrada')
     }else if (res['cod'] == '200') {
         console.log('Cidade encontrada', res.name)
+
+        const date = new Date()
+        const hour = date.getUTCHours() + (res.timezone / -60 / -60)
+        const minute = date.getUTCMinutes()
+        const second = date.getUTCSeconds()
+        console.log(hour >= 24 ? hour - 24 : hour, minute, second)
+        outHour.innerText = `${hour >= 24 ? hour - 24 : hour}:${minute}:${second}`
+
+        const day = hour >= 24 ? date.getUTCDate() + 1 : date.getUTCDate()
+        const month = date.getUTCMonth() + 1
+        const year = date.getUTCFullYear()
+
+        //console.log(day < 10 ? "0"+day : day, month < 10 ? "0"+month : month, year)
+        outDate.innerText = `${day < 10 ? "0"+day : day}/${month < 10 ? "0"+month : month}/${year}`
+
         place.value = res.name
+        outTemp.innerText = res.main.temp.toFixed(1)
+        outFell.innerText = res.main.feels_like.toFixed(1)
+        outMin.innerText = res.main.temp_min.toFixed(1)
+        outMax.innerText = res.main.temp_max.toFixed(1)
     } else {
         console.log('Houve algum erro :(')
     }
 
     return res
+
 }
 
 document.addEventListener('DOMContentLoaded', async () => {
